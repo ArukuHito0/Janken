5e293b79fd0d2ae89c4f3f3e30a834b8910fcf69
ayumuOhara <takeshita0205ayumu@gmail.com>
2026-04-13 00:38:22 +0900

    Update FormFields.cs

--- Janken/Assets/Scripts/FormFields.cs
+++ Janken/Assets/Scripts/FormFields.cs
@@ -5,7 +5,7 @@ public static class FormFields
 {
     public static string GetFormURL(string fileName)
     {
-        return "http://localhost/Janken/" + fileName + ".php";
+        return "http://localhost/Janken-Backend/" + fileName + ".php";
     }
 
     public static readonly string roomId = "room_id";
